14476bf14eae046010aeae6c0ef171b623b7514e
NathanThiery <nthiery99@gmail.com>
2025-09-17 10:34:12 +0200

    Add Class ThirtyTwoBitSets

--- src/FirstCompressionMethod.java
+++ src/FirstCompressionMethod.java
@@ -2,12 +2,13 @@ import java.util.BitSet;
 
 
 public class FirstCompressionMethod {
-    private BitSet bits;
+    private ThirtyTwoBitSets paquetsOf32;
 
     public FirstCompressionMethod(int[] ints) {
+        int i;
         // Espace commentaires
         Writter.fine_log("Construction of FirstCompressionMethod class :");
-        for(int i=0; i < ints.length ; i++){
+        for(i=0; i < ints.length ; i++){
             Writter.fine_log( "\tInteger [" + i + "] = {" + ints[i] + "}");
             Writter.finer_log("\t\t[" + String.format("%32s", Integer.toBinaryString(ints[i])).replace(' ', '0') + "]");
             Writter.finer_log("\t\tnumberOfTrailingZeros = " + (Integer.SIZE - Integer.numberOfLeadingZeros(ints[i])));
@@ -17,9 +18,12 @@ public class FirstCompressionMethod {
         Writter.info_log("\nnumberOfTrailingZeros of the Highest Number is positionned as bit N°" + maxOneBit);
 
         //@TODO : Représenter les entiers de 'ints' en chaine de maxOneBit dans un array d'entier (Ex : 3 = [00101], ainsi de suite)
+        int[] tabOfInts = extractRightBitsArray(ints, maxOneBit);
+        Writter.info_log("\nTableau créé pour être passé à ThirtyTwoBitSets");
+
 
         //@TODO : Construire un tableau de paquets de 32 Bits avec l'appel de la classe ThirtyTwoBitSets
-
+        paquetsOf32 = new ThirtyTwoBitSets(tabOfInts, maxOneBit);
 
     }
 
@@ -36,6 +40,19 @@ public class FirstCompressionMethod {
             }
         }
         return max;
+    }
+
+    // Méthode pour traiter un tableau complet
+    private int[] extractRightBitsArray(int[] arr, int x) {
+        int[] result = new int[arr.length * x];
+        for (int i = 0; i < arr.length; i++) {
+            Writter.fine_log("\nInt[" + arr[i] + "] - Range [" + (i*x) + ";" + (i*x + (x-1)) + "]");
+            for(int j = 0 ; j < x ; j++){
+                result[i * x + j] = (arr[i] >> (x-1) - j) & 1;
+                Writter.finer_log("\t >" + result[i * x + j] + "<");
+            }
+        }
+        return result;
     }
 
 
--- src/ThirtyTwoBitSets.java
+++ src/ThirtyTwoBitSets.java
@@ -8,26 +8,34 @@ public class ThirtyTwoBitSets {
         int actualNbBitSet;
 
         // Number of paquet of 32 Bytes to create in order to be able to stock everything
-        final int lastNbBitSet = divArrondiSup((32 / CompressedBitSize), (ints.length / CompressedBitSize));
+        final int lastNbBitSet ;
+        if(ints.length > 32){
+            lastNbBitSet = divArrondiSup((32 / CompressedBitSize), (ints.length / CompressedBitSize));
+        }else{lastNbBitSet = 1;}
+
         Writter.info_log("\nIntegers given are supposed to be distributed into [" + lastNbBitSet + "] paquets of Size 32 bytes.");
         // -1 because array starts from 0
-        bs = new BitSet[lastNbBitSet-1];
+        bs = new BitSet[lastNbBitSet];
         // Declaration of 32 Bytes paquets
-        for(i = 0 ; i < lastNbBitSet-1 ; i++){
+        for(i = 0 ; i <= lastNbBitSet-1 ; i++){
             bs[i] = new BitSet(32);
         }
-        Writter.fine_log("Integers represented by consecutive [" + CompressedBitSize + "] bytes in an array of total [" + ints.length + "] bytes.\n\t>> " + ints.toString() + " <<");
+        Writter.fine_log("Integers represented by consecutive [" + CompressedBitSize + "] bytes in an array of total [" + ints.length + "] bytes.");
 
 
         for(actualNbBitSet = 0 ; actualNbBitSet < lastNbBitSet ; actualNbBitSet ++){
+            Writter.finer_log("\tConstruction of paquet [" + actualNbBitSet + "] out of [" + (lastNbBitSet-1) + "]");
             if(32 % CompressedBitSize != 0){
                 int lastIntegerOffset = 32 - (32%CompressedBitSize);
-                for(i=0 ; i <= lastIntegerOffset ; i++){
-                    bs[actualNbBitSet].set(32 - i, ints[i + (32*actualNbBitSet)]);
+                Writter.finest_log("\t[" + lastIntegerOffset + "] out of [32] bytes CAN be used in paquet [" + actualNbBitSet + "].");
+                Writter.finest_log("\t[" + ints.length + "] out of [32] bytes WILL be used in paquet [" + actualNbBitSet + "].");
+                for(i=0 ; i < lastIntegerOffset && (i + (31 * actualNbBitSet)) < ints.length; i++){
+                    Writter.finest_log("\t\tPaquet["+actualNbBitSet+"]["+(31-i)+"] = [" + ints[i+(31*actualNbBitSet)] + "]");
+                    bs[actualNbBitSet].set(31 - i, ints[i + (31*actualNbBitSet)] != 0);
                 }
-                if(i <= 32 ){
-                    for(i=i ; i <=32 ; i++){
-                        bs[actualNbBitSet].set(32 - i, false);
+                if(i < 31 ){
+                    for(i=i ; i <=31 ; i++){
+                        bs[actualNbBitSet].set(31 - i, false);
                     }
                 }
             }else{
@@ -36,6 +44,13 @@ public class ThirtyTwoBitSets {
                 }
             }
         }
+
+        Writter.info_log("End of the creation of [" + lastNbBitSet + "] paquet(s) of 32 Bytes.");
+        for(i=0; i < lastNbBitSet ; i++){
+            Writter.fine_log("Paquet[" + i + "] - 32 Bytes");
+                Writter.finer_log("\t>" + bs[i].toString());
+        }
+
 
     }
 
